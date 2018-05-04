09ef0149464c834e70462947e6f8a226e24b1d4f
Andrew Street <andrew_street@live.com>
2018-05-03 18:23:09 -0600

    deployed to AWS-Lambda

--- handler.js
+++ handler.js
@@ -1,16 +1,14 @@
-'use strict';
+const request = require('axios');
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const { differenceWith, isEqual } = require('lodash');
+const {extractListingsFromHTML} = require('./helpers');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.getDonkeyJobs = (event, context, callback) => {
+  request('https://www.thedonkeysanctuary.org.uk/vacancies')
+    .then(({data}) => {
+      const jobs = extractListingsFromHTML(data);
+      callback(null, {jobs});
+    })
+    .catch(callback);
 };
